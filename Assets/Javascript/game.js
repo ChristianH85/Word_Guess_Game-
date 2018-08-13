387f822c4a3895eefd7a28f3cbd719b92281f036
Christian Henry <chenry5724@gmail.com>
2018-08-13 14:32:50 -0500

    organize

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,55 @@
+let guessingteams= [
+            "Arsenal",
+            "Bournemouth",
+            "Brighton and Hove Albion",
+            "Burnley",
+            "Cardiff City",
+            "Chelsea",
+            "Crystal Palace",
+            "Everton",
+            "Fulham",
+            "Huddersfield Town",
+            "Leicester City",
+            "Liverpool",
+            "Manchester City",
+            "Manchester United",
+            "Newcastle",
+            "Southampton",
+            "Tottenham",
+            "Watford",
+            "West Ham United",
+            "Wolverhampton"
+]
+
+let guessingletters= [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z"
+
+]
+let wins=(0)
+let remainingGuesses= (7)
+let 
