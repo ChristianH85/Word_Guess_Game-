f3ce816bb18f4fff1323b104839e5e9ea4ea428b
Christian Henry <chenry5724@gmail.com>
2018-08-18 00:56:45 -0500

    psychic

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,55 +1,60 @@
-let guessingteams= [
-            "Arsenal",
-            "Bournemouth",
-            "Brighton and Hove Albion",
-            "Burnley",
-            "Cardiff City",
-            "Chelsea",
-            "Crystal Palace",
-            "Everton",
-            "Fulham",
-            "Huddersfield Town",
-            "Leicester City",
-            "Liverpool",
-            "Manchester City",
-            "Manchester United",
-            "Newcastle",
-            "Southampton",
-            "Tottenham",
-            "Watford",
-            "West Ham United",
-            "Wolverhampton"
-]
+let letterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let wins = 0;
+let lost = 0;
+let guessesLeft = 10;
+let lettersGuessed = [];
+let mysteryLetter = [];
+let compChoice = letterChoices[Math.floor(Math.random() * letterChoices.length)];
 
-let guessingletters= [
-            "a",
-            "b",
-            "c",
-            "d",
-            "e",
-            "f",
-            "g",
-            "h",
-            "i",
-            "j",
-            "k",
-            "l",
-            "m",
-            "n",
-            "o",
-            "p",
-            "q",
-            "r",
-            "s",
-            "t",
-            "u",
-            "v",
-            "w",
-            "x",
-            "y",
-            "z"
+let newCompChoice = function(){
+    compChoice = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+}
+//restart game with  leters and guesses reset
+let restart = function(){
+    guessesLeft=10;
+    lettersGuessed=[];
+    mysteryLetter = [];
+    newCompChoice();
 
-]
-let wins=(0)
-let remainingGuesses= (7)
-let 
+    
+}
+
+//start game on key press
+document.onkeydown = function(event){
+    
+    console.log(compChoice);
+    let playerGuess= event.key;
+    console.log(playerGuess);
+    lettersGuessed.push(playerGuess);
+    console.log(lettersGuessed[0]);
+    mysteryLetter.push(compChoice);
+    console.log(mysteryLetter[0]);
+
+
+//game function
+
+//document.querySelector(".guessed").innerHtml = lettersGuessed;
+if(guessesLeft > 0 && playerGuess !== compChoice){
+    guessesLeft--;
+    document.querySelector(".guesses").innerHTML = guessesLeft;
+    document.querySelector(".guessed").innerHTML = lettersGuessed;
+}
+else if(guessesLeft===0){
+    lost++;
+    document.querySelector(".loss").innerHTML = lost;    
+    alert("You Lose")
+    restart();
+}   
+
+else if(playerGuess === mysteryLetter[0]){
+    wins++;
+    guessesLeft=0
+    document.querySelector(".wins").innerHTML = wins;
+    alert("You Win")
+    restart();
+
+}
+
+}
+
+
